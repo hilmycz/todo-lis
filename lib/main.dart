346799cb384ca12cd7d5bf4602f09c376a8e58c8
hilmycz <hilmyjilancz@gmail.com>
2024-10-10 19:47:24 +0700

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:new_flutter/splash_screen.dart';
+
+void main(){
+  runApp(const MyApp());
+
+}
+
+ class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'todo app',
+      home: SplashScreen(),
+    );
+  }
+}
